1bd93b80410d798835a28fabe4086147a8f9226d
Sara Hänzi (sszhas) <sara.haenzi@zuerich.ch>
2023-10-26 11:53:49 +0200

    do not load data in ui and server, check if data_vector exists

--- R/app_server.R
+++ R/app_server.R
@@ -6,12 +6,11 @@
 #'
 #' @noRd
 app_server <- function(input, output, session) {
-  # get data with function
-  data_vector <- get_data()
+  # data is loaded upon loading of the package of this app in utils_load_data
   
   # Your application server logic
-  if(is.null(data_vector)){
-    # if data is null then there is nothing displayed in the server
+  if(!exists("data_vector") || is.null(data_vector)){
+    # if data is null then there is nothing to be calculated in the server
   } else {
     mod_area_server(id = "area_zones", 
                     zones = data_vector[["zones"]])
