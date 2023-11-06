02e4567839bf29a0d0cb7d955315bf6a935b7023
sszmea <aline.metzler@zuerich.ch>
2023-11-06 16:53:46 +0100

    automatic gitlab deployment

---
+++ deploy.R
@@ -0,0 +1,12 @@
+# Authenticate
+rsconnect::setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
+                          token = Sys.getenv("TOKEN"),
+                          secret = Sys.getenv("SECRET"))
+# Deploy
+rsconnect::deployApp(forceUpdate = TRUE,
+                     appName = Sys.getenv("APP_NAME"),
+                     appFiles = c(
+                       "R",
+                       "www",
+                       "app.R"
+                     ))
