77e171cfeadbb66c678da9a7ba313621c1a90d19
sszmea <aline.metzler@zuerich.ch>
2024-07-26 11:48:06 +0200

    logic inverted in app_server

--- R/app_server.R
+++ R/app_server.R
@@ -9,9 +9,7 @@ app_server <- function(input, output, session) {
   # data is loaded upon loading of the package of this app in utils_load_data
   
   # Your application server logic
-  if(!exists("data_vector") || is.null(data_vector)){
-    # if data is null then there is nothing to be calculated in the server
-  } else {
+  if (exists("data_vector") && !is.null(data_vector)) {
     mod_area_server(id = "area_zones", 
                     zones = data_vector[["zones"]], 
                     choice_app = 1)
