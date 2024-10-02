2df69b90f207e9ed741b1d75e2614f8b34c9f8d9
sszmea <aline.metzler@zuerich.ch>
2024-10-02 16:57:44 +0200

    tests for data load

--- tests/testthat/test-utils_data_load.R
+++ tests/testthat/test-utils_data_load.R
@@ -1,36 +1,79 @@
 test_that("utils to load data", {
   data_vector <- data_load()
   
-  # there should be no NAs in data_vector
-  expect_true(sum(is.na(data_vector[[1]]$AnzVZA)) == 0)
-  expect_true(sum(is.na(data_vector[[2]]$AnzVZA)) == 0)
+  # there should be 7 dfs in data_vector
+  expect_true(length(data_vector) == 7)
   
-  expect_snapshot_value(prepare_data(data_vector[[1]], data_vector[[2]]), style = "json2")
+  # there should be no NAs in data_vector
+  expect_true(sum(is.na(data_vector[[1]]$Typ)) == 0)
+  expect_true(sum(is.na(data_vector[[2]]$Typ)) == 0)
+  expect_true(sum(is.na(data_vector[[3]]$Typ)) == 0)
+  expect_true(sum(is.na(data_vector[[4]]$Typ)) == 0)
+  expect_true(sum(is.na(data_vector[[5]]$StrasseLang)) == 0)
+  expect_true(sum(is.na(data_vector[[6]]$Typ)) == 0)
+  expect_true(sum(is.na(data_vector[[7]]$Typ)) == 0)
+  
+  expect_snapshot_value(data_vector[[1]], style = "json2")
   
   # check column names
-  expect_named(prepare_data(data_vector[[1]], data_vector[[2]]), 
-               c("Jahr",
-                 "RaumSort",
-                 "RaumLang" ,
-                 "BrancheSort",
-                 "BrancheCd",
-                 "BrancheLang",
-                 "RechtsformSort",
-                 "RechtsformLang",
-                 "BetriebsgrSort",
-                 "BetriebsgrLang",
-                 "Arbeitsstaetten", 
-                 "AnzBesch",
-                 "AnzBeschW",
-                 "AnzBeschM", 
-                 "AnzVZA" , 
-                 "AnzVZAW",
-                 "AnzVZAM"))
+  expect_named(data_vector[[1]], 
+               c("Typ",
+                 "PreisreiheSort",
+                 "PreisreiheLang",
+                 "ArtSort",
+                 "ArtLang",
+                 "GebietSort",
+                 "GebietLang",
+                 "Jahr",
+                 "BZO",
+                 "ALLE",
+                 "ZE",
+                 "KE",
+                 "QU",
+                 "W2",
+                 "W23",
+                 "W34",
+                 "W45",
+                 "W56"))
+  
+  expect_named(data_vector[[4]], 
+               c("Typ",
+                 "QuarCd",
+                 "QuarLang",
+                 "ZoneSort",
+                 "ZoneLang",
+                 "Jahr",
+                 "FrQmBodenGanzeLieg",
+                 "FrQmBodenStwE",
+                 "FrQmBodenAlleHA",
+                 "FrQmBodenNettoGanzeLieg",
+                 "FrQmBodenNettoStwE",
+                 "FrQmBodenNettoAlleHA"))
+  
+  expect_named(data_vector[[6]], 
+               c("Typ",
+                 "PreisreiheSort",
+                 "PreisreiheLang",
+                 "ArtSort",
+                 "ArtLang",
+                 "GebietSort",
+                 "GebietLang",
+                 "Jahr",
+                 "EFH",
+                 "MFH",
+                 "WHG",
+                 "UWH",
+                 "NB",
+                 "UNB",
+                 "IGZ",
+                 "UG"))
   
   
   # check data type
   expect_s3_class(
-    prepare_data(data_vector[[1]], data_vector[[2]]),
+    data_vector[[1]],
     "data.frame"
   )
+  
+  
 })
